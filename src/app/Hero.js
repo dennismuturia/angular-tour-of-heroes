5e3e52f2dc72b838368cc22ca7aeaea5fce2ddd0
dennismuturia <dmuturia2922@gmail.com>
2018-05-15 13:58:10 +0300

    Added a heroes component that I did not add previously

---
+++ src/app/Hero.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Created by Dennis Muturia on 5/14/2018.
+ */
+var Hero = /** @class */ (function () {
+    function Hero() {
+    }
+    return Hero;
+}());
+exports.Hero = Hero;
